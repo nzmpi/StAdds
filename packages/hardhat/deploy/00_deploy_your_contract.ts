32ce56a1d94b0c8ce0eb022d1569ead522ce35ba
nzmpi <nizami.abdulov@gmail.com>
2023-07-29 19:02:58 +0400

    upd

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -28,7 +28,7 @@ const StAdds: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
-    autoMine: true,
+    //autoMine: true,
   });
 
   // Get the deployed contract
